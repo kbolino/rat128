ce78eeff5f6fbc79ed0a3b87997641187a04ef04
Kristian Bolino <kbolino@gmail.com>
2023-12-30 17:02:13 -0500

    Improve readability of complex code

--- rat128.go
+++ rat128.go
@@ -188,30 +188,68 @@ func (x N) Cmp(y N) int {
 // Add adds x and y and returns the result.
 // Add panics if the result would overflow.
 func (x N) Add(y N) N {
-	m1, n1 := x.Num(), x.Den()
-	m2, n2 := y.Num(), y.Den()
-	// TODO detect overflow more precisely
-	if abs64(m1) < math.MaxInt32 && abs64(m2) < math.MaxInt32 && n1 < math.MaxInt32 && n2 < math.MaxInt32 {
-		return New(m1*n2+m2*n1, n1*n2)
+	mx, nx := x.Num(), x.Den()
+	my, ny := y.Num(), y.Den()
+
+	// Use naive arithmetic if we can.
+	// TODO improve overflow check if possible, e.g. using bit-length sums?
+	if abs64(mx) < math.MaxInt32 && abs64(my) < math.MaxInt32 && nx < math.MaxInt32 && ny < math.MaxInt32 {
+		// Overflow analysis:
+		//
+		// Define len(x) as the number of bits used to represent abs(x); we
+		// can ignore the sign here because it always takes up 1 bit in the
+		// result regardless of the operation or the size of the operands.
+		//
+		// Next, the if statement guarantees us that len(abs(mx)) <= 31,
+		// len(abs(my)) <= 31, len(nx) <= 31, and len(ny) <= 31.
+		//
+		// Therefore, len(mx*ny) <= 62 and len(my*nx) <= 62 since the product
+		// of two n-bit numbers takes at most 2*n bits.
+		//
+		// Finally, len(mx*ny+my*nx) <= 63 since the sum of two n-bit numbers
+		// takes at most n+1 bits. Thus, the numerator cannot overflow.
+		//
+		// It also follows that the denominator cannot overflow, since
+		// len(nx*ny) <= 62.
+		return New(mx*ny+my*nx, nx*ny)
 	}
-	s1, s2 := sgn64(m1), sgn64(m2)
+
+	// We can't use simple arithmetic if we've made it this far, because the
+	// intermediate values might overflow. Instead, we will use wider ops.
+	// But first, let's check the signs to skip unnecessary work.
+	s1, s2 := sgn64(mx), sgn64(my)
 	if s1 == 0 {
 		return y
 	} else if s2 == 0 {
 		return x
 	}
-	mh1, ml1 := bits.Mul64(uint64(abs64(m1)), uint64(n2))
-	mh2, ml2 := bits.Mul64(uint64(abs64(m2)), uint64(n1))
-	nh, nl := bits.Mul64(uint64(n1), uint64(n2))
+
+	// Multiply the mx*ny, my*nx, and nx*ny terms with 128-bit precision.
+	// From here on out, h is for "high bits" and l is for "low bits".
+	m1h, m1l := bits.Mul64(uint64(abs64(mx)), uint64(ny))
+	m2h, m2l := bits.Mul64(uint64(abs64(my)), uint64(nx))
+	nh, nl := bits.Mul64(uint64(nx), uint64(ny))
+
+	// Compute the full numerator m (mh:ml) with wide arithmetic.
+	//
+	// There are six cases to consider with respect to the signs and sizes of
+	// m1 (m1h:m1l) and m2 (m2h:m2l):
+	//
+	// - the signs are the same and positive; then       m =   |m1| + |m2|
+	// - the signs are the same and negative; then       m = -(|m1| + |m2|)
+	// - the signs differ, m1 > 0, and |m1| > |m2|; then m =   |m1| - |m2|
+	// - the signs differ, m1 > 0, and |m1| < |m2|; then m = -(|m2| - |m1|)
+	// - the signs differ, m1 < 0, and |m1| > |m2|; then m = -(|m1| - |m2|)
+	// - the signs differ, m1 < 0, and |m1| < |m2|; then m =   |m2| - |m1|
 	var ml, mh uint64
 	sgn := int64(1)
 	if s1 == s2 {
 		if s1 < 0 {
 			sgn = -1
 		}
-		var mlc, mhc uint64
-		ml, mlc = bits.Add64(ml1, ml2, 0)
-		mh, mhc = bits.Add64(mh1, mh2, mlc)
+		var mlc, mhc uint64 // c is for "carry"
+		ml, mlc = bits.Add64(m1l, m2l, 0)
+		mh, mhc = bits.Add64(m1h, m2h, mlc)
 		if mhc != 0 {
 			panic(ErrNumOverflow)
 		}
@@ -221,26 +259,29 @@ func (x N) Add(y N) N {
 			sgn = -sgn
 		}
 		// |m1| < |m2|
-		if mh2 > mh1 || (mh2 == mh1 && ml2 > ml1) {
-			mh1, mh2 = mh2, mh1
-			ml1, ml2 = ml2, ml1
+		if m2h > m1h || (m2h == m1h && m2l > m1l) {
+			m1h, m2h = m2h, m1h
+			m1l, m2l = m2l, m1l
 			sgn = -sgn
 		}
-		var mlb, mhb uint64
-		ml, mlb = bits.Sub64(ml1, ml2, 0)
-		mh, mhb = bits.Sub64(mh1, mh2, mlb)
+		var mlb, mhb uint64 // b is for "borrow"
+		ml, mlb = bits.Sub64(m1l, m2l, 0)
+		mh, mhb = bits.Sub64(m1h, m2h, mlb)
 		if mhb != 0 {
 			panic(ErrNumOverflow)
 		}
 	}
-	d := GCD(n1, n2)
-	n, _ := bits.Div64(nh, nl, uint64(d))
-	if n > math.MaxInt64 {
-		panic(ErrDenOverflow)
-	}
+
+	// Finally, find the GCD of the numerator and denominator and divide it out
+	// to reduce the result before the final overflow checks.
+	d := GCD(nx, ny)
 	m, _ := bits.Div64(mh, ml, uint64(d))
 	if m > math.MaxInt64 {
 		panic(ErrNumOverflow)
+	}
+	n, _ := bits.Div64(nh, nl, uint64(d))
+	if n > math.MaxInt64 {
+		panic(ErrDenOverflow)
 	}
 	return New(sgn*int64(m), int64(n))
 }
@@ -256,27 +297,43 @@ func (x N) Sub(y N) N {
 // Mul multiplies x and y and returns the result.
 // Mul panics if the result would overflow.
 func (x N) Mul(y N) N {
+	// Compute the sign of the result.
 	sgn := int64(x.Sign() * y.Sign())
 	if sgn == 0 {
 		return N{}
 	}
-	m1, n1 := abs64(x.Num()), x.Den()
-	m2, n2 := abs64(y.Num()), y.Den()
-	d12, d21 := GCD(m1, n2), GCD(m2, n1)
-	if d12 != 1 {
-		m1, n2 = m1/d12, n2/d12
+	// We can ignore the operand signs now that we know the result sign, so we
+	// work only with absolute values for simplicity.
+	mx, nx := abs64(x.Num()), x.Den()
+	my, ny := abs64(y.Num()), y.Den()
+
+	// Next, we reduce the fractions by their cross-GCDs to avoid overflow.
+	// Even though x and y are already reduced, their product may introduce
+	// factors from each that aren't present in the other.
+	// Since the result is going to be (mx*my)/(nx*ny), we can divide out
+	// GCD(mx, ny) and GCD(my, nx) without changing the value.
+	if d := GCD(mx, ny); d != 1 {
+		mx, ny = mx/d, ny/d
 	}
-	if d21 != 1 {
-		m2, n1 = m2/d21, n1/d21
+	if d := GCD(my, nx); d != 1 {
+		my, nx = my/d, nx/d
 	}
-	if m1 < math.MaxInt32 && m2 < math.MaxInt32 && n1 < math.MaxInt32 && n2 < math.MaxInt32 {
-		return New(sgn*m1*m2, n1*n2)
+
+	// Use naive multiplication if we can.
+	// TODO improve the overflow check if possible, e.g. len(mx)+len(my)<=63?
+	if mx < math.MaxInt32 && my < math.MaxInt32 && nx < math.MaxInt32 && ny < math.MaxInt32 {
+		// See Add for a detailed overflow analysis; suffice it to say that
+		// the above if statement protects us from overflow here.
+		return New(sgn*mx*my, nx*ny)
 	}
-	mh, ml := bits.Mul64(uint64(m1), uint64(m2))
+
+	// At this point, we can't trust naive multiplication to not overflow, so
+	// we use wide arithmetic to check for overflow.
+	mh, ml := bits.Mul64(uint64(mx), uint64(my))
 	if mh > 0 || ml > math.MaxInt64 {
 		panic(ErrNumOverflow)
 	}
-	nh, nl := bits.Mul64(uint64(n1), uint64(n2))
+	nh, nl := bits.Mul64(uint64(nx), uint64(ny))
 	if nh > 0 || nl > math.MaxInt64 {
 		panic(ErrDenOverflow)
 	}
@@ -300,17 +357,25 @@ func (x N) String() string {
 // value is exactly equal to x; otherwise, it is the closest approximation.
 func (x N) Float64() (v float64, exact bool) {
 	m, n := x.Num(), x.Den()
+
+	// check for zero, trivial case
 	if m == 0 {
 		return 0, true
 	}
+
+	// integers are exact as long as they fit in the mantissa
 	prec := bits.Len64(uint64(abs64(m)))
 	if n == 1 {
 		return float64(m), prec <= 53
 	}
+
+	// non-integers are exact as long as the numerator fits in the mantissa
+	// and the denominator is a power of two
 	nIsPow2 := bits.OnesCount64(uint64(n)) == 1
 	return float64(m) / float64(n), prec <= 53 && nIsPow2
 }
 
+// reduce returns x in lowest terms.
 func (x N) reduce() N {
 	if x.m == 0 {
 		return N{}
@@ -321,6 +386,7 @@ func (x N) reduce() N {
 	return N{sgn * (m / d), (n / d) - 1}
 }
 
+// abs64 returns the absolute value of x.
 func abs64(x int64) int64 {
 	if x < 0 {
 		return -x
@@ -328,6 +394,7 @@ func abs64(x int64) int64 {
 	return x
 }
 
+// sgn64 returns -1 if x < 0, 0 if x == 0, and 1 if x > 0.
 func sgn64(x int64) int64 {
 	if x == 0 {
 		return 0
